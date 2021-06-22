ab1e0f5095155897a4b10ce5515f44174763c875
CJ Jurgens <cjjurgens@unomaha.edu>
2021-06-22 17:02:49 -0500

    Updating to a real vue application and adding card component.

---
+++ main-application/src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="card">
+  <h1>This is a card</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+}
+</script>
+
+<style scoped>
+.card{
+  padding: 20px;
+  width: 200px;
+  height: 150px;
+  border:2px solid;
+  border-color:black;
+  border-radius: 25px;
+  background: gray;
+  color: white;
+}
+</style>
