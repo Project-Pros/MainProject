9593356d4cc9f9a9f8d46c887bc2ec3ace7924c6
CJ Jurgens <cjjurgens@unomaha.edu>
2021-07-13 17:06:14 -0500

    adding in the public html file again

--- main-application/src/pages/kanbanBoard.vue
+++ main-application/src/pages/kanbanBoard.vue
@@ -1,4 +1,39 @@
-   <h3>To Do</h3>
+<template>
+  <div class="container mt-4">
+    <div >
+      <div >
+        <b-form-input
+            id="input-2"
+            v-model="addTask"
+            required
+            placeholder="Add New Task"
+        ></b-form-input>
+        <b-button @click="add">Add</b-button>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-3">
+        <div class="p-2 alert alert-light">
+          <h3>Back Log List</h3>
+          <draggable
+              class="list-group kanbanColumn"
+              :list="arrBackLog"
+              group="tasks"
+          >
+            <div
+                class="list-group-item"
+                v-for="element in arrBackLog"
+                :key="element.name"
+            >
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="p-2 alert alert-success">
+        <h3>To Do</h3>
 
            <draggable
              class="list-group kanbanColumn"
